d76876ea3f41fab1a912669f2733453cea6fa835
Lenovo <kroemer.jens@gmx.de>
2021-03-10 01:17:59 +0100

    Loading and exploring data in Python

--- src/basicDataExploration.py
+++ src/basicDataExploration.py
@@ -1,1 +1,9 @@
-print("hello")
+import pandas as pd
+import os
+
+# save filepath to variable for easier access
+melbourne_file_path = os.getcwd() + '/data/melb_data.csv'
+# read the data and store data in DataFrame titled melbourne_data
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# print a summary of the data in Melbourne data
+print(melbourne_data.describe())
